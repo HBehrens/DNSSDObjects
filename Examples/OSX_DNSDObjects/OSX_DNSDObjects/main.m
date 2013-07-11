07a645d212663b2c7008fb150cf279a7ca989de9
Heiko Behrens <HeikoBehrens@gmx.de>
2013-07-11 14:50:14 +0200

    restructure directory layout and re-create OSX app to match future directory layout

--- Examples/OSX_DNSDObjects/OSX_DNSDObjects/main.m
+++ Examples/OSX_DNSDObjects/OSX_DNSDObjects/main.m
@@ -1,55 +1,55 @@
 /*
-    File:       main.m
-
-    Contains:   Command line tool main.
-
-    Written by: DTS
-
-    Copyright:  Copyright (c) 2011 Apple Inc. All Rights Reserved.
-
-    Disclaimer: IMPORTANT: This Apple software is supplied to you by Apple Inc.
-                ("Apple") in consideration of your agreement to the following
-                terms, and your use, installation, modification or
-                redistribution of this Apple software constitutes acceptance of
-                these terms.  If you do not agree with these terms, please do
-                not use, install, modify or redistribute this Apple software.
-
-                In consideration of your agreement to abide by the following
-                terms, and subject to these terms, Apple grants you a personal,
-                non-exclusive license, under Apple's copyrights in this
-                original Apple software (the "Apple Software"), to use,
-                reproduce, modify and redistribute the Apple Software, with or
-                without modifications, in source and/or binary forms; provided
-                that if you redistribute the Apple Software in its entirety and
-                without modifications, you must retain this notice and the
-                following text and disclaimers in all such redistributions of
-                the Apple Software. Neither the name, trademarks, service marks
-                or logos of Apple Inc. may be used to endorse or promote
-                products derived from the Apple Software without specific prior
-                written permission from Apple.  Except as expressly stated in
-                this notice, no other rights or licenses, express or implied,
-                are granted by Apple herein, including but not limited to any
-                patent rights that may be infringed by your derivative works or
-                by other works in which the Apple Software may be incorporated.
-
-                The Apple Software is provided by Apple on an "AS IS" basis. 
-                APPLE MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING
-                WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT,
-                MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING
-                THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE OR IN
-                COMBINATION WITH YOUR PRODUCTS.
-
-                IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT,
-                INCIDENTAL OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-                TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-                DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY
-                OUT OF THE USE, REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION
-                OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER UNDER THEORY
-                OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR
-                OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF
-                SUCH DAMAGE.
-
-*/
+ File:       main.m
+ 
+ Contains:   Command line tool main.
+ 
+ Written by: DTS
+ 
+ Copyright:  Copyright (c) 2011 Apple Inc. All Rights Reserved.
+ 
+ Disclaimer: IMPORTANT: This Apple software is supplied to you by Apple Inc.
+ ("Apple") in consideration of your agreement to the following
+ terms, and your use, installation, modification or
+ redistribution of this Apple software constitutes acceptance of
+ these terms.  If you do not agree with these terms, please do
+ not use, install, modify or redistribute this Apple software.
+ 
+ In consideration of your agreement to abide by the following
+ terms, and subject to these terms, Apple grants you a personal,
+ non-exclusive license, under Apple's copyrights in this
+ original Apple software (the "Apple Software"), to use,
+ reproduce, modify and redistribute the Apple Software, with or
+ without modifications, in source and/or binary forms; provided
+ that if you redistribute the Apple Software in its entirety and
+ without modifications, you must retain this notice and the
+ following text and disclaimers in all such redistributions of
+ the Apple Software. Neither the name, trademarks, service marks
+ or logos of Apple Inc. may be used to endorse or promote
+ products derived from the Apple Software without specific prior
+ written permission from Apple.  Except as expressly stated in
+ this notice, no other rights or licenses, express or implied,
+ are granted by Apple herein, including but not limited to any
+ patent rights that may be infringed by your derivative works or
+ by other works in which the Apple Software may be incorporated.
+ 
+ The Apple Software is provided by Apple on an "AS IS" basis.
+ APPLE MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING
+ WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT,
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING
+ THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE OR IN
+ COMBINATION WITH YOUR PRODUCTS.
+ 
+ IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT,
+ INCIDENTAL OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY
+ OUT OF THE USE, REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION
+ OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER UNDER THEORY
+ OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR
+ OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+ 
+ */
 
 #import <Foundation/Foundation.h>
 
@@ -58,13 +58,13 @@
 #import "DNSSDService.h"
 
 static NSString * Quote(NSString * str)
-    // Returns a quoted form of the string.  This isn't intended to be super-clever, 
-    // for example, it doesn't escape any single quotes in the string.
+// Returns a quoted form of the string.  This isn't intended to be super-clever,
+// for example, it doesn't escape any single quotes in the string.
 {
     return [NSString stringWithFormat:@"'%@'", (str == nil) ? @"" : str];
 }
 
-// MainBase is an abstract base class from which each of the main classes are derived. 
+// MainBase is an abstract base class from which each of the main classes are derived.
 // It's only that to support starting and stopping the event loop.
 
 @interface MainBase : NSObject
@@ -106,18 +106,18 @@ static NSString * Quote(NSString * str)
     
     self.registration = [[[DNSSDRegistration alloc] initWithDomain:nil type:self.type name:nil port:self.port] autorelease];
     assert(self.registration != nil);
-
+    
     self.registration.delegate = self;
-
+    
     [self.registration start];
-
+    
     [super run];
 }
 
 - (void)dnssdRegistrationWillRegister:(DNSSDRegistration *)sender
 {
     assert(sender == self.registration);
-    #pragma unused(sender)
+#pragma unused(sender)
     
     NSLog(@"will register %@ / %@ / %@", Quote(self.registration.name), Quote(self.registration.type), Quote(self.registration.domain));
 }
@@ -125,17 +125,17 @@ static NSString * Quote(NSString * str)
 - (void)dnssdRegistrationDidRegister:(DNSSDRegistration *)sender
 {
     assert(sender == self.registration);
-    #pragma unused(sender)
-
+#pragma unused(sender)
+    
     NSLog(@"registered as %@ / %@ / %@", Quote(self.registration.registeredName), Quote(self.registration.type), Quote(self.registration.registeredDomain));
 }
 
 - (void)dnssdRegistration:(DNSSDRegistration *)sender didNotRegister:(NSError *)error
 {
     assert(sender == self.registration);
-    #pragma unused(sender)
+#pragma unused(sender)
     assert(error != nil);
-
+    
     NSLog(@"did not register (%@ / %zd)", [error domain], (ssize_t) [error code]);
     [self quit];
 }
@@ -143,8 +143,8 @@ static NSString * Quote(NSString * str)
 - (void)dnssdRegistrationDidStop:(DNSSDRegistration *)sender
 {
     assert(sender == self.registration);
-    #pragma unused(sender)
-
+#pragma unused(sender)
+    
     NSLog(@"stopped");
 }
 
@@ -192,31 +192,31 @@ static NSString * Quote(NSString * str)
 - (void)dnssdBrowserWillBrowse:(DNSSDBrowser *)browser
 {
     assert(browser == self.browser);
-    #pragma unused(browser)
-
+#pragma unused(browser)
+    
     NSLog(@"will browse %@ / %@", Quote(self.browser.type), Quote(self.browser.domain));
 }
 
 - (void)dnssdBrowser:(DNSSDBrowser *)browser didAddService:(DNSSDService *)service moreComing:(BOOL)moreComing
 {
     assert(browser == self.browser);
-    #pragma unused(browser)
-
+#pragma unused(browser)
+    
     NSLog(@"   add service %@ / %@ / %@%s", Quote(service.name), Quote(service.type), Quote(service.domain), moreComing ? " ..." : "");
 }
 
 - (void)dnssdBrowser:(DNSSDBrowser *)browser didRemoveService:(DNSSDService *)service moreComing:(BOOL)moreComing
 {
     assert(browser == self.browser);
-    #pragma unused(browser)
-
+#pragma unused(browser)
+    
     NSLog(@"remove service %@ / %@ / %@%s", Quote(service.name), Quote(service.type), Quote(service.domain), moreComing ? " ..." : "");
 }
 
 - (void)dnssdBrowser:(DNSSDBrowser *)browser didNotBrowse:(NSError *)error
 {
     assert(browser == self.browser);
-    #pragma unused(browser)
+#pragma unused(browser)
     assert(error != nil);
     
     NSLog(@"did not browse (%@ / %zd)", [error domain], (ssize_t) [error code]);
@@ -226,7 +226,7 @@ static NSString * Quote(NSString * str)
 - (void)dnssdBrowserDidStopBrowse:(DNSSDBrowser *)browser
 {
     assert(browser == self.browser);
-    #pragma unused(browser)
+#pragma unused(browser)
     NSLog(@"stopped");
 }
 
@@ -278,14 +278,14 @@ static NSString * Quote(NSString * str)
 - (void)dnssdServiceWillResolve:(DNSSDService *)service
 {
     assert(service == self.service);
-    #pragma unused(service)
+#pragma unused(service)
     NSLog(@"will resolve %@ / %@ / %@", Quote(self.service.name), Quote(self.service.type), Quote(self.service.domain));
 }
 
 - (void)dnssdServiceDidResolveAddress:(DNSSDService *)service
 {
     assert(service == self.service);
-    #pragma unused(service)
+#pragma unused(service)
     NSLog(@"did resolve to %@:%zu", service.resolvedHost, (size_t) service.resolvedPort);
     [self quit];
 }
@@ -293,7 +293,7 @@ static NSString * Quote(NSString * str)
 - (void)dnssdService:(DNSSDService *)service didNotResolve:(NSError *)error
 {
     assert(service == self.service);
-    #pragma unused(service)
+#pragma unused(service)
     NSLog(@"did not resolve (%@ / %zd)", [error domain], (ssize_t) [error code]);
     [self quit];
 }
@@ -301,7 +301,7 @@ static NSString * Quote(NSString * str)
 - (void)dnssdServiceDidStop:(DNSSDService *)service
 {
     assert(service == self.service);
-    #pragma unused(service)
+#pragma unused(service)
     NSLog(@"stopped");
 }
 
@@ -332,29 +332,29 @@ static NSString * Quote(NSString * str)
 
 int main(int argc, char **argv)
 {
-    #pragma unused(argc)
-    #pragma unused(argv)
+#pragma unused(argc)
+#pragma unused(argv)
     int                 retVal;
     MainBase *          mainObj;
-
+    
     @autoreleasepool {
         retVal = EXIT_FAILURE;
         if ( (argc == 4) && (strcasecmp(argv[1], "-r") == 0) ) {
             RegisterMain *  registerObj;
-
+            
             registerObj = [[[RegisterMain alloc] init] autorelease];
             assert(registerObj != nil);
             
             registerObj.type = [NSString stringWithUTF8String:argv[2]];
             registerObj.port = (NSUInteger) atoi(argv[3]);
-            if ( (registerObj.type != nil) && ([registerObj.type length] != 0) && ! [registerObj.type hasPrefix:@"."] && 
-                 (registerObj.port != 0) && (registerObj.port < 65536) ) {
+            if ( (registerObj.type != nil) && ([registerObj.type length] != 0) && ! [registerObj.type hasPrefix:@"."] &&
+                (registerObj.port != 0) && (registerObj.port < 65536) ) {
                 mainObj = registerObj;
                 retVal = EXIT_SUCCESS;
             }
         } else if ( (argc == 3) && (strcasecmp(argv[1], "-b") == 0) ) {
             BrowseMain *    browseObj;
-
+            
             browseObj = [[[BrowseMain alloc] init] autorelease];
             assert(browseObj != nil);
             
@@ -365,16 +365,16 @@ int main(int argc, char **argv)
             }
         } else if ( (argc == 5) && (strcasecmp(argv[1], "-l") == 0) ) {
             ResolveMain *   resolveObj;
-
+            
             resolveObj = [[[ResolveMain alloc] init] autorelease];
             assert(resolveObj != nil);
             
             resolveObj.name   = [NSString stringWithUTF8String:argv[2]];
             resolveObj.type   = [NSString stringWithUTF8String:argv[3]];
             resolveObj.domain = [NSString stringWithUTF8String:argv[4]];
-            if ( (resolveObj.name   != nil) && ([resolveObj.name   length] != 0) && 
-                 (resolveObj.type   != nil) && ([resolveObj.type   length] != 0) && ! [resolveObj.type   hasPrefix:@"."] && 
-                 (resolveObj.domain != nil) && ([resolveObj.domain length] != 0) && ! [resolveObj.domain hasPrefix:@"."] ) {
+            if ( (resolveObj.name   != nil) && ([resolveObj.name   length] != 0) &&
+                (resolveObj.type   != nil) && ([resolveObj.type   length] != 0) && ! [resolveObj.type   hasPrefix:@"."] &&
+                (resolveObj.domain != nil) && ([resolveObj.domain length] != 0) && ! [resolveObj.domain hasPrefix:@"."] ) {
                 mainObj = resolveObj;
                 retVal = EXIT_SUCCESS;
             }
@@ -388,6 +388,8 @@ int main(int argc, char **argv)
             [mainObj run];
         }
     }
-
+    
     return retVal;
 }
+
+
