dc3a84dc93a06f5c36c1d5812c94bd6c1eaaa10c
Heiko Behrens <HeikoBehrens@gmx.de>
2013-07-12 10:10:35 +0200

    again, trying to get travis working

--- Scripts/build-osx.sh
+++ Scripts/build-osx.sh
@@ -4,4 +4,4 @@ set -e
 cd $(dirname $0)
 
 cd ../Examples/OSX_DNSDObjects
-xctool -workspace OSX_DNSDObjects.xcworkspace -scheme OSX_DNSDObjects clean build 
+xctool -workspace OSX_DNSDObjects.xcworkspace -scheme OSX_DNSDObjects build 
